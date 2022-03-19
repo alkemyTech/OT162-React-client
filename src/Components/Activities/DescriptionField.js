7cbac4517a160e971ec13bef2e4bb996dfb5d002
Lucas <lavendanozarza@alu.frlp.utn.edu.ar>
2022-03-19 02:19:46 -0300

    Ckeditor toolbar modified (reduced amount of buttons)

--- src/Components/Activities/DescriptionField.js
+++ src/Components/Activities/DescriptionField.js
@@ -4,8 +4,39 @@ const DescriptionField = (props) => {
   return (
     <CKEditor
       name={props.field.name}
+      editor="classic"
       onChange={props.onChange}
       data={props.field.value}
+      config={{
+        toolbar: [
+          ["Source", "-", "NewPage", "Preview", "-", "Templates"],
+          [
+            "Cut",
+            "Copy",
+            "Paste",
+            "PasteText",
+            "-",
+            "Undo",
+            "Redo",
+            "-",
+            "Bold",
+            "Italic",
+            "Underline",
+            "-",
+            "NumberedList",
+            "BulletedList",
+            "-",
+            "Link",
+            "Unlink",
+            "Anchor",
+            "-",
+            "Image",
+            "Table",
+          ],
+          "/",
+          ["Styles", "Format", "Font", "FontSize", "-", "TextColor", "BGColor"],
+        ],
+      }}
     />
   );
 };
