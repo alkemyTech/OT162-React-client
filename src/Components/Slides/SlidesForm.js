de6151b68ceac3afc2650eb93fc86f87ee88ed78
Michel San Martin <mikeemmanuel91@gmail.com>
2022-03-20 11:13:44 -0300

    adding file input and CKEditor

--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import CKEditor from 'ckeditor4-react';
 import '../FormStyles.css';
 
 const SlidesForm = () => {
@@ -10,7 +11,8 @@ const SlidesForm = () => {
     const handleChange = (e) => {
         if(e.target.name === 'name'){
             setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
+        } 
+        if(e.target.name === 'description'){
             setInitialValues({...initialValues, description: e.target.value})
         }
     }
@@ -23,7 +25,18 @@ const SlidesForm = () => {
     return (
         <form className="form-container" onSubmit={handleSubmit}>
             <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Slide Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write the description"></input>
+            {/* <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write the description"></input>           */}
+            <CKEditor 
+                className="input-field" 
+                name="description" 
+                value={initialValues.description} 
+                data="Write the description"
+                // onBeforeLoad={CKEditor =>{ console.log(CKEditor)}}
+                onChange={handleChange}
+                // onFocus={(data) =>console.log(data)}
+                // onSubmit={(data) => console.log(data)}
+                />  
+            <input className="input-filed" type="file" name="image"/>
             <button className="submit-btn" type="submit">Send</button>
         </form>
     );
