f027bc9191837f1b4e38cad8caaaa3e414507d39
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-03-20 22:22:27 -0300

    Added backoffice view and a few cards.

---
+++ src/Backoffice/Backoffice.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import './Backoffice.css';
+
+const Backoffice = () => {
+    return (
+        <Box mt={6}>
+            <Grid container rowSpacing={2} columnSpacing={{ xs: 12, sm: 12, md: 12 }} sx={{justifyContent: "center"}}>
+                <Grid item sx={{ minWidth: 250 }}>
+                    <Card sx={{ minWidth: 250, boxShadow: 3, borderRadius: '6px' }} variant="outlined">
+                        <CardContent>
+                            <Typography align="center" variation="primary" sx={{color: '#76b5fa', fontWeight: 'bold'}}>
+                                Novedades
+                                <Button variant="contained" sx={{backgroundColor: '#319795'}}>
+                                    Ir
+                                </Button>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item sx={{ minWidth: 250 }}>
+                    <Card sx={{ minWidth: 250, boxShadow: 3, borderRadius: '6px' }} variant="outlined">
+                        <CardContent>
+                            <Typography align="center" variation="primary" sx={{color: '#76b5fa', fontWeight: 'bold'}}>
+                                Novedades
+                                <Button variant="contained" sx={{backgroundColor: '#319795'}}>
+                                    Ir
+                                </Button>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item sx={{ minWidth: 250 }}>
+                    <Card sx={{ minWidth: 250, boxShadow: 3, borderRadius: '6px' }} variant="outlined">
+                        <CardContent>
+                            <Typography align="center" variation="primary" sx={{color: '#76b5fa', fontWeight: 'bold'}}>
+                                Novedades
+                                <Button variant="contained" sx={{backgroundColor: '#319795'}}>
+                                    Ir
+                                </Button>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item sx={{ minWidth: 250 }}>
+                    <Card sx={{ minWidth: 250, boxShadow: 3, borderRadius: '6px' }} variant="outlined">
+                        <CardContent>
+                            <Typography align="center" variation="primary" sx={{color: '#76b5fa', fontWeight: 'bold'}}>
+                                Novedades
+                                <Button variant="contained" sx={{backgroundColor: '#319795'}}>
+                                    Ir
+                                </Button>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item sx={{ minWidth: 250 }}>
+                    <Card sx={{ minWidth: 250, boxShadow: 3, borderRadius: '6px' }} variant="outlined">
+                        <CardContent>
+                            <Typography align="center" variation="primary" sx={{color: '#76b5fa', fontWeight: 'bold'}}>
+                                Novedades
+                                <Button variant="contained" sx={{backgroundColor: '#319795'}}>
+                                    Ir
+                                </Button>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item sx={{ minWidth: 250 }}>
+                    <Card sx={{ minWidth: 250, boxShadow: 3, borderRadius: '6px' }} variant="outlined">
+                        <CardContent>
+                            <Typography align="center" variation="primary" sx={{color: '#76b5fa', fontWeight: 'bold'}}>
+                                Novedades
+                                <Button variant="contained" sx={{backgroundColor: '#319795'}}>
+                                    Ir
+                                </Button>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item sx={{ minWidth: 250 }}>
+                    <Card sx={{ minWidth: 250, boxShadow: 3, borderRadius: '6px' }} variant="outlined">
+                        <CardContent>
+                            <Typography align="center" variation="primary" sx={{color: '#76b5fa', fontWeight: 'bold'}}>
+                                Novedades
+                                <Button variant="contained" sx={{backgroundColor: '#319795'}}>
+                                    Ir
+                                </Button>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item sx={{ minWidth: 250 }}>
+                    <Card sx={{ minWidth: 250, boxShadow: 3, borderRadius: '6px' }} variant="outlined">
+                        <CardContent>
+                            <Typography align="center" variation="primary" sx={{color: '#76b5fa', fontWeight: 'bold'}}>
+                                Novedades
+                                <Button variant="contained" sx={{backgroundColor: '#319795'}}>
+                                    Ir
+                                </Button>
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Box>
+    )
+}
+
+export default Backoffice;
