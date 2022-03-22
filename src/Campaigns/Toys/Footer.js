8a57d5c28f861aaddf47778a872023d6345aba6c
Michel San Martin <mikeemmanuel91@gmail.com>
2022-03-21 23:42:37 -0300

    adding subscribe form and button

--- src/Campaigns/Toys/Footer.js
+++ src/Campaigns/Toys/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import NewsletterForm from '../../features/suscribeNewsletter';
 
 const Footer = () => {
   return (
     <div>
-      
+      <NewsletterForm/>
     </div>
   );
 }
