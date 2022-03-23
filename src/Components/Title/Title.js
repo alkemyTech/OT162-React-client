d555ba33b7b5b4bc6510685dc5fea10ce81da73a
manuelservin <servinchristianmanuel@gmail.com>
2022-03-23 18:57:08 -0300

    updated classes names

--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -5,18 +5,20 @@ import './title.css'
 const Title = ({title, text, img}) => {
   
   return (
-    <div className="hero">
+    <div className="container">
 
-    <div   className='hero__img' style ={ { backgroundImage: `url( ${img ? img : '/images/placeholder/1920x680.png' })`,backgroundRepeat:"no-repeat",
+    <div   className='container__img' style ={ { backgroundImage: `url( ${img ? img : '/images/placeholder/1920x680.png' })`,backgroundRepeat:"no-repeat",
     backgroundSize:"cover", 
     backgroundPosition:"center",
     position: 'relative',
-    height: "50%",
+    height: "50%"
+
+    ,
     } }>
-       <div className='hero__text'> 
+       <div className='container__text'> 
        
-       <h1 className='hero__text__title'>{title}</h1>
-       <p className='hero__text__body'>{text} </p>
+       <h1 className='container__text__title'>{title}</h1>
+       <p className='container__text__body'>{text} </p>
        </div> 
    
 
