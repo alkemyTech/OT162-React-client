2d2513f16d71e9ea4041a8d09b5db7adcb3fd54a
Lucas <lavendanozarza@alu.frlp.utn.edu.ar>
2022-03-24 19:58:18 -0300

    Added Patch method, waiting for getAuth method to be added to main

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,15 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
 const config = {
-    headers: {
-        Group: 01                //Aqui va el ID del equipo!!
-    }
-}
+  headers: {
+    Group: 162, //Aqui va el ID del equipo!!
+  },
+};
 
 const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+  axios
+    .get("https://jsonplaceholder.typicode.com/users", config)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+const Patch = (ruta, obj, id) => {
+  axios
+    .patch(`${ruta}/${id}`, obj, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: GetAuth(),
+      },
+    })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
 
-export default Get
+export { Get, Patch };
