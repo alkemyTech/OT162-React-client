16425891a1710ffeb1edc0d446a091a0df4e8f80
manuelservin <servinchristianmanuel@gmail.com>
2022-03-24 17:21:36 -0300

    aggregate get method

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -2,14 +2,30 @@ import axios from 'axios';
 
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        Group: 162                //Aqui va el ID del equipo!!
     }
 }
+const baseUrl = 'https://ongapi.alkemy.org/api'
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
+const Get = async (endpoint, id) => {
+    try{
+        if(id){
+          const resp= await axios.get(`${baseUrl}/${endpoint}/${id}`,config)
+          return resp.data
+          
+    
+        } else{
+            const resp= await  axios.get(`${baseUrl}/${endpoint}`, config)
+            
+           return resp.data
+          
+        }
+
+    }
+    catch(error){
+            console.log(error)
+
+    }
 }
 
 export default Get
