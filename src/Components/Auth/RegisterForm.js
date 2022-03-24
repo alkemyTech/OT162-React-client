ff7fa287356c2173c1e0da57b044155869f458df
Angel Morales <angelmoraleszero@gmail.com>
2022-03-24 18:26:28 -0300

    Register form with error message

--- src/Components/Auth/RegisterForm.js
+++ src/Components/Auth/RegisterForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../FormStyles.css";
-import swal from "sweetalert";
+import { ErrorMessage, Formik } from "formik";
 
 const RegisterForm = () => {
   const [initialValues, setInitialValues] = useState({
@@ -10,7 +10,6 @@ const RegisterForm = () => {
     password: "",
     confirmPassword: "",
   });
-
   const handleChange = (e) => {
     switch (e.target.name) {
       case "name":
@@ -33,93 +32,123 @@ const RegisterForm = () => {
     }
   };
 
-  const handleSubmit = (e) => {
+  const handleEdit = (e) => {
     e.preventDefault();
-    if (initialValues) {
-      const { password, confirmPassword } = initialValues;
-      const specialCaracRegex = /\W/i;
-      const numbersRegex = /[0-9]/;
-      const errors = {};
-
-      Object.values(initialValues).map((value) => {
-        if (value === "" || undefined || null) {
-          errors.form = "todos los campos son obligatorios";
-          swal(errors.form, {
-            button: "Cerrar",
-          });
-        }
-        return errors;
-      });
-
-      if (!errors.form) {
-        if (password !== confirmPassword) {
-          errors.password = "las contraseñas deben ser iguales";
-          swal(errors.password, {
-            button: "Cerrar",
-          });
-        } else if (
-          !password.length >= 6 ||
-          !specialCaracRegex.test(password) ||
-          !numbersRegex.test(password)
-        ) {
-          errors.password =
-            "Las contraseñas deben ser de al menos 6 caracteres y contener al menos 1 simbolo, 1 letra y 1 numero";
-          swal(errors.password, {
-            button: "Cerrar",
-          });
-        }
-      }
-    }
-
     localStorage.setItem("token", "tokenValueExample");
   };
 
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <input
-        className="input-field"
-        type="text"
-        name="name"
-        value={initialValues.name}
-        onChange={handleChange}
-        placeholder="Enter name"
-      ></input>
-      <input
-        className="input-field"
-        type="text"
-        name="lastName"
-        value={initialValues.lastName}
-        onChange={handleChange}
-        placeholder="Enter last name"
-      ></input>
-      <input
-        className="input-field"
-        type="text"
-        name="email"
-        value={initialValues.email}
-        onChange={handleChange}
-        placeholder="Enter email"
-      ></input>
-      <input
-        className="input-field"
-        type="text"
-        name="password"
-        value={initialValues.password}
-        onChange={handleChange}
-        placeholder="Enter password"
-      ></input>
-      <input
-        className="input-field"
-        type="text"
-        name="confirmPassword"
-        value={initialValues.confirmPassword}
-        onChange={handleChange}
-        placeholder="Repeat password"
-      ></input>
-      <button className="submit-btn" type="submit">
-        Register
-      </button>
-    </form>
+    <div>
+      {""}
+      <Formik
+        initialValues={initialValues}
+        validate={() => {
+          const { password, confirmPassword, name, email, lastName } =
+            initialValues;
+          const specialCaracRegex = /\W/i;
+          const numbersRegex = /[0-9]/;
+          const errors = {};
+
+          if (!name) {
+            errors.name = "Campo obligatorio";
+          } else if (!lastName) {
+            errors.lastName = "Campo obligatorio";
+          } else if (!confirmPassword) {
+            errors.confirmPassword = "Campo obligatorio";
+          } else if (!email) {
+            errors.email = "Campo obligatorio";
+          } else if (!password) {
+            errors.password = "Campo obligatorio";
+          } else if (password !== confirmPassword) {
+            errors.password = "las contraseñas deben ser iguales";
+          } else if (
+            !password.length >= 6 ||
+            !specialCaracRegex.test(password) ||
+            !numbersRegex.test(password)
+          ) {
+            errors.password =
+              "Las contraseñas deben ser de al menos 6 caracteres y contener al menos 1 simbolo, 1 letra y 1 numero";
+          }
+
+          console.log(errors);
+          return errors;
+        }}
+        onSubmit={handleEdit}
+      >
+        {({ handleSubmit }) => (
+          <form className="form-container" onSubmit={handleSubmit}>
+            <input
+              className="input-field"
+              type="text"
+              name="name"
+              value={initialValues.name}
+              onChange={handleChange}
+              placeholder="Enter name"
+            ></input>
+            <ErrorMessage
+              name="name"
+              component="div"
+              className="invalid-feedback"
+            />
+            <input
+              className="input-field"
+              type="text"
+              name="lastName"
+              value={initialValues.lastName}
+              onChange={handleChange}
+              placeholder="Enter last name"
+            ></input>
+            <ErrorMessage
+              name="lastName"
+              component="div"
+              className="invalid-feedback"
+            />
+            <input
+              className="input-field"
+              type="text"
+              name="email"
+              value={initialValues.email}
+              onChange={handleChange}
+              placeholder="Enter email"
+            ></input>
+            <ErrorMessage
+              name="email"
+              component="div"
+              className="invalid-feedback"
+            />
+            <input
+              className="input-field"
+              type="text"
+              name="password"
+              value={initialValues.password}
+              onChange={handleChange}
+              placeholder="Enter password"
+            ></input>
+            <ErrorMessage
+              name="password"
+              component="div"
+              className="invalid-feedback"
+            />
+            <input
+              className="input-field"
+              type="text"
+              name="confirmPassword"
+              value={initialValues.confirmPassword}
+              onChange={handleChange}
+              placeholder="Repeat password"
+            ></input>
+            <ErrorMessage
+              name="confirmPassword"
+              component="div"
+              className="invalid-feedback"
+            />
+            <button className="submit-btn" type="submit">
+              Register
+            </button>
+          </form>
+        )}
+      </Formik>
+    </div>
   );
 };
 
