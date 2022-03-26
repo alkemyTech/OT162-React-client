57041a192a1407f9d8fe36de44b2ebb86b7c13b8
Michel San Martin <mikeemmanuel91@gmail.com>
2022-03-26 12:31:48 -0300

    Basic table template

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
 import ActivityDetail from './Components/Activities/Detail/ActivityDetail';
-import UserList from './Components/Users/UserList';
+import UserList from './Components/Users/UserList/UserList';
 
 function App() {
   return (
---
+++ src/Components/Users/UserList/UserList.js
@@ -0,0 +1,37 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const UserList = () => {
+    return(
+        <div>
+            <h1>UserList</h1>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>User name</TableCell>
+                            <TableCell>Email</TableCell>
+                            <TableCell>Acions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell>User Example</TableCell>
+                            <TableCell>User@example.com</TableCell>
+                            <TableCell>
+                                
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+         </div>
+    )
+}
+
+export default UserList;
