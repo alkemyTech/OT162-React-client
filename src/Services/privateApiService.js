98ec33f14f7b7655725c3019d93752ae627cc93d
Angel Morales <angelmoraleszero@gmail.com>
2022-03-28 17:45:45 -0300

    Get Requests centralized

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,20 +1,31 @@
-import axios from 'axios';
+import axios from "axios";
 
 const config = {
-    headers: {
-        Group: 162               //Aqui va el ID del equipo!!
-    }
-}
+  headers: {
+    Group: 162, //Aqui va el ID del equipo!!
+  },
+};
+// "https://jsonplaceholder.typicode.com/users"
+const Get = (url, id) => {
+  const auth = GetAuth();
+  config.headers.authorization = auth;
+  let httpURL;
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
-}
+  if (id) {
+    httpURL = url + "/" + id;
+  } else {
+    httpURL = url;
+  }
+  console.log(httpURL);
+  axios
+    .get(httpURL, config)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
 
 const GetAuth = () => {
-    let token = localStorage.getItem('token');
-    return token !== null ? `Bearer ${token}` : null;
-}
+  let token = localStorage.getItem("token");
+  return token !== null ? `Bearer ${token}` : null;
+};
 
-export {Get, GetAuth};
+export { Get, GetAuth };
