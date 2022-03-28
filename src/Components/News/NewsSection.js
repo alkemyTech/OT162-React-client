607189af97a7fe812430445b38721b108c73a501
leocomeron <leocomeron@gmail.com>
2022-03-28 18:44:42 -0300

    add card component as a comment, waiting for merge

--- src/Components/News/NewsSection.js
+++ src/Components/News/NewsSection.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Title from "../Title/Title";
+import "./NewsSection.css";
+// import Card from "../Cards/Cars.js"; //add Card component when it's merged
 
-const CARD_COMPONENT = [
-  "First card component",
-  "second card component",
-  "third card component",
+const CARD_DUMMY_DATA = [
+  {
+    image:
+      "https://st3.depositphotos.com/5080845/15395/i/1600/depositphotos_153952346-stock-photo-aquarium-yawning-gold-fish.jpg",
+    title: "First card component",
+    description: "Alguna descripción",
+    placeholder: "Algúnplaceholder",
+  },
+  {
+    image:
+      "https://st3.depositphotos.com/5080845/15395/i/1600/depositphotos_153952346-stock-photo-aquarium-yawning-gold-fish.jpg",
+    title: "Second card component",
+    description: "Alguna descripción 2",
+    placeholder: "Algún placeholder 2",
+  },
 ];
+
 const newsSection = () => {
   return (
     <div>
@@ -18,9 +32,17 @@ const newsSection = () => {
           "https://site.groupe-psa.com/content/uploads/sites/3/2016/12/white-background-2.jpg"
         }
       />
-      {CARD_COMPONENT.map((card) => (
-        <div>{card}</div>
-      ))}
+      <div className="cardsContainer">
+        {CARD_DUMMY_DATA.map((card) => (
+          <div>{card.title}</div> //Change the div component to the Card component and pass the props
+          // <Card
+          //   image={card.image}
+          //   title={card.title}
+          //   description={card.description}
+          //   placeholder={card.placeholder}
+          // />
+        ))}
+      </div>
     </div>
   );
 };
