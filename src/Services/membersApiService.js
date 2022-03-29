95ba18d5f129a473db2d6e4ab565e87ce6439d6a
manuelservin <servinchristianmanuel@gmail.com>
2022-03-29 08:09:04 -0300

    GET, POST, PUT, DEL added

---
+++ src/Services/membersApiService.js
@@ -0,0 +1,21 @@
+import rutas from "../config/rutas";
+import { Delete, Get, Post, Put } from "./privateApiService";
+
+
+const getMembers = ()=> Get(rutas.MEMBERS_URL);
+
+const getMemberById = ()=> Get(rutas.MEMBERS_URL, id);
+
+const addNewMember = (member)=> Post(rutas.MEMBERS_URL, member)
+
+const updateMember = (id, newData)=> Put(rutas.MEMBERS_URL, id, newData)
+
+const deleteMember = (id)=> Delete(rutas.MEMBERS_URL,id)
+
+export {
+    getMembers,
+    getMemberById,
+    addNewMember,
+    updateMember,
+    deleteMember,
+}
