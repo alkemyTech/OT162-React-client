c9d90a38f08b9af4530b1b79ab4dd07852d736f9
leocomeron <leocomeron@gmail.com>
2022-03-30 17:50:59 -0300

    Add Card component and change styles of Title and Card

--- src/Components/News/NewsSection.js
+++ src/Components/News/NewsSection.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Title from "../Title/Title";
 import "./NewsSection.css";
-// import Card from "../Cards/Cars.js"; //add Card component when it's merged
+import Card from "../Cards/Card";
+import { Grid } from "@mui/material";
 
 const CARD_DUMMY_DATA = [
   {
@@ -18,6 +19,20 @@ const CARD_DUMMY_DATA = [
     description: "Alguna descripción 2",
     placeholder: "Algún placeholder 2",
   },
+  {
+    image:
+      "https://st3.depositphotos.com/5080845/15395/i/1600/depositphotos_153952346-stock-photo-aquarium-yawning-gold-fish.jpg",
+    title: "Third card component",
+    description: "Alguna descripción 3",
+    placeholder: "Algún placeholder 3",
+  },
+  {
+    image:
+      "https://st3.depositphotos.com/5080845/15395/i/1600/depositphotos_153952346-stock-photo-aquarium-yawning-gold-fish.jpg",
+    title: "Fourth card component",
+    description: "Alguna descripción 4",
+    placeholder: "Algún placeholder 4",
+  },
 ];
 
 const newsSection = () => {
@@ -32,17 +47,21 @@ const newsSection = () => {
           "https://site.groupe-psa.com/content/uploads/sites/3/2016/12/white-background-2.jpg"
         }
       />
-      <div className="cardsContainer">
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        sx={{ margin: 3 }}
+      >
         {CARD_DUMMY_DATA.map((card) => (
-          <div>{card.title}</div> //Change the div component to the Card component and pass the props
-          // <Card
-          //   image={card.image}
-          //   title={card.title}
-          //   description={card.description}
-          //   placeholder={card.placeholder}
-          // />
+          <Card
+            image={card.image}
+            title={card.title}
+            description={card.description}
+            placeholder={card.placeholder}
+          />
         ))}
-      </div>
+      </Grid>
     </div>
   );
 };
