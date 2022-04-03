c8f0b1750ccf03dfafbf54b432c469c6ee57995a
Lucas <lavendanozarza@alu.frlp.utn.edu.ar>
2022-04-03 18:10:33 -0300

    Added loader and alerts on error to Nosotros and Home components

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import rutas from "../../config/rutas";
+import Loading from "../Utilities/Loading";
 import Carousel from "./Carousel";
 import HomeTitle from "./HomeTitle";
 import NewsList from "./NewsList";
+import { errorAlert } from "../../../features/alerts/alerts";
+
 const Home = () => {
   const [slides, setSlides] = useState([]);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     axios
       .get(rutas.GET_SLIDES_URL)
       .then((res) => {
@@ -14,14 +20,24 @@ const Home = () => {
         setSlides(res.data.data);
       })
       .catch((err) => {
+        errorAlert(
+          "Error",
+          "An error has occurred while getting data from server.",
+          "Ok"
+        );
         console.log(err);
-      });
+      })
+      .finally(() => setLoading(false));
   }, []);
 
   return (
     <div>
       <HomeTitle />
-      <Carousel slides={slides} background={"#90a4ae"} />
+      {loading ? (
+        <Loading />
+      ) : (
+        <Carousel slides={slides} background={"#90a4ae"} />
+      )}
       {/* background is Carousel's backgroundColor  */}
       <NewsList />
     </div>
