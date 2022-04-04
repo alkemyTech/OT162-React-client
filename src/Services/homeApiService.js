6a20e4af41134687f9d8ec4f27bbdf62545f11ee
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-04-03 21:48:33 -0300

    Now it is possible to upload slides

--- src/Services/homeApiService.js
+++ src/Services/homeApiService.js
@@ -2,23 +2,33 @@ import {Get, Post} from './publicApiService';
 import axios from 'axios';
 
 const PostSliders = (values) => {
-    console.log(values);
-    Post('slides', {
-        "name": values.photoName1,
-        "description": "string",
-        "image": values.photo1,
-    });
-    Post('slides', {
-        "name": values.photoName2,
-        "description": "string",
-        "image": values.photo2,
-    });
-    Post('slides', {
-        "name": values.photoName3,
-        "description": "string",
-        "image": values.photo3,
-    });
-    console.log('Sending slides...');
+    async function getBase64() {
+        await axios.get(values.photo1, {responseType: 'arraybuffer'})
+        .then(response => {
+            Post('slides', {
+                "name": values.photoName1,
+                "description": "string",
+                "image": "data:image/jpeg;base64," + Buffer.from(response.data, 'binary').toString('base64'),
+            });
+        })
+        await axios.get(values.photo2, {responseType: 'arraybuffer'})
+        .then(response => {
+            Post('slides', {
+                "name": values.photoName2,
+                "description": "string",
+                "image": "data:image/jpeg;base64," + Buffer.from(response.data, 'binary').toString('base64'),
+            });
+        })
+        await axios.get(values.photo3, {responseType: 'arraybuffer'})
+        .then(response => {
+            Post('slides', {
+                "name": values.photoName3,
+                "description": "string",
+                "image": "data:image/jpeg;base64," + Buffer.from(response.data, 'binary').toString('base64'),
+            });
+        })
+    }
+    getBase64()
 }
 
 export {PostSliders};
