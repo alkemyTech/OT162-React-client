79650aba8ee5f0b912aeb083759b86543209bc60
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-04-03 23:50:16 -0300

    Refactored homeApiService

--- src/Services/homeApiService.js
+++ src/Services/homeApiService.js
@@ -1,34 +1,31 @@
 import {Get, Post} from './publicApiService';
 import axios from 'axios';
 
-const PostSliders = (values) => {
-    async function getBase64() {
-        await axios.get(values.photo1, {responseType: 'arraybuffer'})
-        .then(response => {
-            Post('slides', {
-                "name": values.photoName1,
-                "description": "string",
-                "image": "data:image/jpeg;base64," + Buffer.from(response.data, 'binary').toString('base64'),
-            });
-        })
-        await axios.get(values.photo2, {responseType: 'arraybuffer'})
-        .then(response => {
-            Post('slides', {
-                "name": values.photoName2,
-                "description": "string",
-                "image": "data:image/jpeg;base64," + Buffer.from(response.data, 'binary').toString('base64'),
-            });
-        })
-        await axios.get(values.photo3, {responseType: 'arraybuffer'})
-        .then(response => {
-            Post('slides', {
-                "name": values.photoName3,
-                "description": "string",
-                "image": "data:image/jpeg;base64," + Buffer.from(response.data, 'binary').toString('base64'),
-            });
-        })
-    }
-    getBase64()
+const PostSliders = async (values) => {
+    await axios.get(values.photo1, {responseType: 'arraybuffer'})
+    .then(response => {
+        Post('slides', {
+            "name": values.photoName1,
+            "description": "string",
+            "image": "data:image/jpeg;base64," + Buffer.from(response.data, 'binary').toString('base64'),
+        });
+    });
+    await axios.get(values.photo2, {responseType: 'arraybuffer'})
+    .then(response => {
+        Post('slides', {
+            "name": values.photoName2,
+            "description": "string",
+            "image": "data:image/jpeg;base64," + Buffer.from(response.data, 'binary').toString('base64'),
+        });
+    });
+    await axios.get(values.photo3, {responseType: 'arraybuffer'})
+    .then(response => {
+        Post('slides', {
+            "name": values.photoName3,
+            "description": "string",
+            "image": "data:image/jpeg;base64," + Buffer.from(response.data, 'binary').toString('base64'),
+        });
+    });
 }
 
 export {PostSliders};
