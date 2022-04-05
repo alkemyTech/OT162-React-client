268207ecbc0e67f6992a655e5113ba3712dd6f4f
Michel San Martin <mikeemmanuel91@gmail.com>
2022-04-05 09:05:56 -0300

    added loading spinner

--- src/Components/Activities/Detail/ActivityContent.js
+++ src/Components/Activities/Detail/ActivityContent.js
@@ -1,19 +1,34 @@
 import { errorAlert } from '../../../features/alerts/alerts';
+import { useEffect, useState } from 'react';
+import Loading from '../../Utilities/Loading';
 
 const ActivityContent = (description) => {
+
+    const [loading, setLoading] = useState(true);
+
     let HTMLString = description
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, []);
 
     if(HTMLString.value !== undefined){
         function createMarkup(){
             return{__html: (HTMLString)}
         }
         return(
-            <div dangerouslySetInnerHTML={createMarkup()}/>
+            <div>
+                { loading ? Loading() : <div dangerouslySetInnerHTML={createMarkup()}/>}
+            </div>
         )
     }else{
         errorAlert('Ha ocurrido un error', 'No hay actividades que mostrar', 'Continuar...')
         return(
-            <h3>Here well be display a selected activity</h3>
+            <div>
+                { loading? Loading() : <h3>Aqui veras las próximas acitivades</h3>}
+            </div>            
         )
     }
 }
