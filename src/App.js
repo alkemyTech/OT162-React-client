0385adcde5ef11654cc3d80c32c80011a27c58ae
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-04-06 16:39:43 -0300

    Pulling last changes from main

--- src/App.js
+++ src/App.js
@@ -31,7 +31,6 @@ import NewsSection from "./Components/News/NewsSection";
 import BackofficeMembersList from './Components/Members/BackofficeMembersList';
 import News from './Components/News/News';
 import SiteDataForm from './Components/SiteDataForm/SiteDataForm'
-import BackofficeMembersList from "./Components/Members/BackofficeMembersList";
 import {
   GetNews,
   GetSingleNews,
