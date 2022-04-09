b33e754d2083f53c7a8cf9f2989f8f5fa3b1e462
Lucas <lavendanozarza@alu.frlp.utn.edu.ar>
2022-04-09 20:35:47 -0300

    Added user reducer, user slice and updated UserLists component

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import slideReducer from "../features/slide/slideSlice";
+import usersSlice from "../features/users/usersSlice";
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     slide: slideReducer,
-    aboutUs: aboutUsReducer,
+    users: usersSlice,
+    // aboutUs: aboutUsReducer,
   },
 });
