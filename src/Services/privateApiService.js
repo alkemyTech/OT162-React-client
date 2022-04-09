b6a47f555ba74fed0b850b7481cec3b3ea22a205
Lucas <lavendanozarza@alu.frlp.utn.edu.ar>
2022-04-09 19:57:29 -0300

    Move auth header from config to each method

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -9,13 +9,18 @@ const config = {
   headers: {
     Group: 162, //Aqui va el ID del equipo!!
     "Content-Type": "application/json",
-    Authorization: GetAuth(),
   },
 };
 
-const Put = (route, id, data) => axios.put(`${route}/${id}`, data, config);
+const Put = (route, id, data) => {
+  config.headers.Authorization = GetAuth();
+  axios.put(`${route}/${id}`, data, config);
+};
 
-const Patch = (ruta, obj, id) => axios.patch(`${ruta}/${id}`, obj, config);
+const Patch = (ruta, obj, id) => {
+  config.headers.Authorization = GetAuth();
+  axios.patch(`${ruta}/${id}`, obj, config);
+};
 
 const Get = (url, id) => {
   let httpURL;
@@ -28,8 +33,14 @@ const Get = (url, id) => {
   return axios.get(httpURL, config);
 };
 
-const Post = (URL, Body) => axios.post(URL, Body, config);
+const Post = (URL, Body) => {
+  config.headers.Authorization = GetAuth();
+  axios.post(URL, Body, config);
+};
 
-const Delete = (path, id) => axios.delete(path + "/" + id, config);
+const Delete = (path, id) => {
+  config.headers.Authorization = GetAuth();
+  axios.delete(path + "/" + id, config);
+};
 
 export { Get, GetAuth, Post, Put, Patch, Delete };
