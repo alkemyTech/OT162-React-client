c2fbdde958c8828e08be0ad35769a21bd28d0f98
Michel San Martin <mikeemmanuel91@gmail.com>
2022-04-12 19:21:32 -0300

    reducer created

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import slideReducer from "../features/slide/slideSlice";
 import aboutUsReducer from "../features/reducers/aboutUsSlice";
+import membersSlice from '../features/reducers/membersSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
     slide: slideReducer,
     aboutUs: aboutUsReducer,
+    members: membersSlice,
   },
 });
