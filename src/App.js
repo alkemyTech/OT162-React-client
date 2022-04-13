f073b8a92183f7fa1abffe078c09a211fd3431cf
leocomeron <leocomeron@gmail.com>
2022-04-12 23:37:18 -0300

    Delete innecesary imports

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from "react";
 import "./App.css";
-import UserForm from "./Components/Users/UsersForm";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Components/Home/index";
 
-import {
-  GetNews,
-  GetSingleNews,
-  PostNews,
-  PutNews,
-  DeleteNews,
-} from "./Services/newsApiService";
 import PublicRoute from "./routes/PublicRoute";
 import PrivateRoute from "./routes/PrivateRoute";
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <PublicRoute />
-        <PrivateRoute />
-      </div>
-    </>
+    <div className="App">
+      <PublicRoute />
+      <PrivateRoute />
+    </div>
   );
 }
 
