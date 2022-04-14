8fe36f5e6728a0a3fec3fa4a3e0b64cfcbad7c83
Lucas <lavendanozarza@alu.frlp.utn.edu.ar>
2022-04-14 18:55:17 -0300

    Added activitiesApiService.js

---
+++ src/Services/activitiesApiService.js
@@ -0,0 +1,16 @@
+import { Put, Post, Delete } from "./privateApiService";
+
+const updateActivity = (url, initialValues) => {
+  let id = initialValues.id;
+  return Put(url, id, initialValues);
+};
+
+const createActivity = (url, initialValues) => {
+  return Post(url, initialValues);
+};
+
+const deleteActivity = (path, activity) => {
+  return Delete(path, activity);
+};
+
+export { updateActivity, createActivity, deleteActivity };
