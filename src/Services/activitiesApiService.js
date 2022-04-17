defb78fb2edf74b4c55a8e7774a30b135208c505
leocomeron <leocomeron@gmail.com>
2022-04-17 12:35:39 -0300

    fix conflicts in activitiesApiService

--- src/Services/activitiesApiService.js
+++ src/Services/activitiesApiService.js
@@ -1,16 +1,17 @@
-import { Put, Post, Patch, Delete } from "./privateApiService";
+import {Put, Post, Patch, Delete} from './privateApiService';
 
-const updateActivity = (url, initialValues) => {
-  let id = initialValues.id;
-  return Put(url, id, initialValues);
-};
 
-const createActivity = (url, initialValues) => {
-  return Post(url, initialValues);
-};
+const updateActivity = (url, initialValues) => {    
+    let id = initialValues.id;
+    return Put(url, id, initialValues)
+}
+
+const createActivity = (url, initialValues) => {   
+    return Post(url, initialValues)
+}
 
 const deleteActivity = (path, activity) => {
-  return Delete(path, activity);
-};
+    return Delete(path, activity)
+}
 
-export { updateActivity, createActivity, deleteActivity };
+export {updateActivity, createActivity, deleteActivity};
