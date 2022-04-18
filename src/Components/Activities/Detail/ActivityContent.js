63f6c658e31daaaebd8a1ee5948dc4a1930d3478
Lucas <playstationgrosa@hotmail.com>
2022-04-18 20:27:32 -0300

    Fix activity detail components

--- src/Components/Activities/Detail/ActivityContent.js
+++ src/Components/Activities/Detail/ActivityContent.js
@@ -1,36 +1,13 @@
-import { errorAlert } from '../../../features/alerts/alerts';
-import { useEffect, useState } from 'react';
-import Loading from '../../Utilities/Loading';
+const ActivityContent = ({ description }) => {
+  return (
+    <>
+      {description ? (
+        <span dangerouslySetInnerHTML={{ __html: description }} />
+      ) : (
+        <span>No hay descripción</span>
+      )}
+    </>
+  );
+};
 
-const ActivityContent = (description) => {
-
-    const [loading, setLoading] = useState(true);
-
-    let HTMLString = description
-
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false);
-        }, 2000);
-    }, []);
-
-    if(HTMLString.value !== undefined){
-        function createMarkup(){
-            return{__html: (HTMLString)}
-        }
-        return(
-            <div>
-                { loading ? Loading() : <div dangerouslySetInnerHTML={createMarkup()}/>}
-            </div>
-        )
-    }else{
-        errorAlert('Ha ocurrido un error', 'No hay actividades que mostrar', 'Continuar...')
-        return(
-            <div>
-                { loading? Loading() : <h3>Aqui veras las próximas acitivades</h3>}
-            </div>            
-        )
-    }
-}
-
-export default ActivityContent
+export default ActivityContent;
--- src/Components/Activities/Detail/ActivityDetail.js
+++ src/Components/Activities/Detail/ActivityDetail.js
@@ -1,14 +1,35 @@
-import React from "react";
+import { useParams } from "react-router-dom";
 import Title from "../../Title/Title";
+import { useState, useEffect } from "react";
+import { getActivitiesById } from "../../../Services/activitiesApiService";
+import Loading from "../../Utilities/Loading";
+import { errorAlert } from "../../../features/alerts/alerts";
 import ActivityContent from "./ActivityContent";
 
 const ActivityDetail = () => {
+  const id = useParams();
+  const [loading, setLoading] = useState(false);
+  const [activity, setActivity] = useState({});
+  useEffect(() => {
+    setLoading(true);
+    getActivitiesById(id.id)
+      .then((response) => setActivity(response.data.data))
+      .catch((error) => errorAlert(error, error.message, "Ok"))
+      .finally(() => setLoading(false));
+  }, [id]);
+
   return (
     <>
-      <Title title="Activity title" />
-      <div className="content">
-        <ActivityContent></ActivityContent>
-      </div>
+      {loading ? (
+        <Loading open={true} />
+      ) : (
+        <>
+          <Title title={activity.name} />
+          <div className="content">
+            <ActivityContent description={activity.description} />
+          </div>
+        </>
+      )}
     </>
   );
 };
