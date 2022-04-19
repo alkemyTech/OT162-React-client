62d9d22f6249a375fe0a04da46bfc28a8aaaade9
Michel San Martin <mikeemmanuel91@gmail.com>
2022-04-18 22:13:29 -0300

    basic layout header

--- src/Campaigns/Toys/Header.js
+++ src/Campaigns/Toys/Header.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Card, Grid } from '@mui/material';
+import logoONG from "../../assets/img/logo-somos-mas.png";
+import campaignLogo from "../../assets/img/logo-toys-campaign.png";
+
+const imageStyle = {
+  width: '150px',
+}
+
+const campaingSlogan = "Slogan de campaña"
 
 const Header = () => {
   return (
     <div>
-      
+      <Card>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="flex-start">
+          <img src={campaignLogo} style={imageStyle}/>
+          <img src={logoONG} style={imageStyle}/>
+          <h2>{campaingSlogan}</h2>
+        </Grid>
+      </Card>
     </div>
   );
 }
