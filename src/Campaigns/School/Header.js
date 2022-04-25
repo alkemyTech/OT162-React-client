43590862b7284505376e14d82dd730b04208c64d
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-04-25 01:05:34 -0300

    Added solid white color to header background for different screen resolution

--- src/Campaigns/School/Header.js
+++ src/Campaigns/School/Header.js
@@ -17,14 +17,32 @@ const Header = () => {
 
   return (
     <ThemeProvider theme={theme}>
-      <Grid container sx={{padding: 5, justifyContent: 'space-between', backgroundColor: {xl: '#ff00008e'}}}>
-        <Grid item xs={12} sm={6} md={4} sx={{ margin: 'auto', justifyContent: "center", alignItems: "center"}}>
+      <Grid 
+        container 
+        sx={{padding: 5, 
+          justifyContent: 'space-between', 
+          backgroundColor: {
+            xs: '#fff', 
+            sm: '#fff', 
+            md: '#fff', 
+            xs: '#fff', 
+            xl: '#ff00008e'}}}>
+        <Grid item 
+          xs={12} 
+          sm={6} 
+          md={4} 
+          sx={{ margin: 'auto', justifyContent: "center", alignItems: "center"}}>
           <img src='images/school-campaign/school-campaign-logo.png' height={150}/>
         </Grid>
-        <Grid item md={4} sx={{ display: { xs: "none", md: "flex" }, justifyContent: "center", alignItems: "center"}}>
+        <Grid item 
+          md={4} 
+          sx={{ display: { xs: "none", md: "flex" }, justifyContent: "center", alignItems: "center"}}>
           <h2>Juguetes por más sonrisas</h2>
         </Grid>
-        <Grid item sm={6} md={4} sx={{ display: {xs: "none", sm: "block"}, justifyContent: "center", alignItems: "center" }}>
+        <Grid item 
+          sm={6} 
+          md={4} 
+          sx={{ display: {xs: "none", sm: "block"}, justifyContent: "center", alignItems: "center" }}>
           <img src='images/school-campaign/LOGO-SOMOS-MAS.png'/>
         </Grid>
       </Grid>
