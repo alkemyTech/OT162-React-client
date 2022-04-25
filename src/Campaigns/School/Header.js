cd88a5311469832f57ea01a97483713af6721179
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-04-25 00:14:23 -0300

    Added school campaign header and its elements

--- src/Campaigns/School/Header.js
+++ src/Campaigns/School/Header.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Grid } from '@mui/material';
 
 const Header = () => {
   return (
-    <div>
-
-    </div>
+    <Grid container sx={{padding: 5, justifyContent: 'space-between'}}>
+      <Grid item xs={12} sm={6} md={4} sx={{ margin: 'auto', justifyContent: "center", alignItems: "center"}}>
+        <img src='images/school-campaign/school-campaign-logo.png' height={150}/>
+      </Grid>
+      <Grid item md={4} sx={{ display: { xs: "none", md: "flex" }, justifyContent: "center", alignItems: "center"}}>
+        <h2>Juguetes por más sonrisas</h2>
+      </Grid>
+      <Grid item sm={6} md={4} sx={{ display: {xs: "none", sm: "block"}, justifyContent: "center", alignItems: "center" }}>
+        <img src='images/school-campaign/LOGO-SOMOS-MAS.png'/>
+      </Grid>
+    </Grid>
   );
 }
  
