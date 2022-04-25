1f5ab420fb8be803e45b18b7c4970beacc5bff8b
Juan Cruz Soto <juancruzlp2013@gmail.com>
2022-04-25 14:25:55 -0300

    private routes redirect control

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Backoffice from "../Backoffice/Backoffice";
 import NewsForm from "../Components/News/NewsForm";
@@ -14,24 +14,23 @@ import HomeForm from "../Components/Backoffice/Organization/HomeForm";
 import ContactForm from "../Components/Contact/ContactForm";
 
 const PrivateRoute = () => {
+
+  const auth = "Adminisstrador"
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/backoffice" element={<Backoffice />} />
-        <Route path="/backoffice/news/create" element={<NewsForm />} />
-        <Route path="/backoffice/create-slide" element={<SlidesForm />} />
-        <Route path="/backoffice/slides" element={<SlidesList />} />
-        <Route path="/backoffice/categories" element={<CategoriesTable />} />
-        <Route path="/backoffice/members/edit/:id" element={<MembersForm />} />
-        <Route
-          path="/backoffice/activities"
-          element={<ActivitiesListBackoffice />}
-        />
-        <Route path="/backoffice/members" element={<BackofficeMembersList />} />
-        <Route path="/backoffice/news" element={<News />} />
-        <Route path="/backoffice/users" element={<UserList />} />
-        <Route path="/backoffice/Organization" element={<HomeForm />} />
-        <Route path="/backoffice/contact/create" element={<ContactForm />} />
+        <Route path="/backoffice" element={auth !== "Administrador" ? <Navigate to="/" /> : <Backoffice />} />
+        <Route path="/backoffice/news/create" element={auth !== "Administrador" ? <Navigate to="/" /> : <NewsForm />} />
+        <Route path="/backoffice/create-slide" element={auth !== "Administrador" ? <Navigate to="/" /> : <SlidesForm />} />
+        <Route path="/backoffice/slides" element={auth !== "Administrador" ? <Navigate to="/" /> : <SlidesList />} />
+        <Route path="/backoffice/categories" element={auth !== "Administrador" ? <Navigate to="/" /> : <CategoriesTable />} />
+        <Route path="/backoffice/members/edit/:id" element={auth !== "Administrador" ? <Navigate to="/" /> : <MembersForm />} />
+        <Route path="/backoffice/activities" element={auth !== "Administrador" ? <Navigate to="/" /> : <ActivitiesListBackoffice />}/>
+        <Route path="/backoffice/members" element={auth !== "Administrador" ? <Navigate to="/" /> : <BackofficeMembersList />} />
+        <Route path="/backoffice/news" element={auth !== "Administrador" ? <Navigate to="/" /> : <News />} />
+        <Route path="/backoffice/users" element={auth !== "Administrador" ? <Navigate to="/" /> : <UserList />} />
+        <Route path="/backoffice/Organization" element={auth !== "Administrador" ? <Navigate to="/" /> : <HomeForm />} />
+        <Route path="/backoffice/contact/create" element={auth !== "Administrador" ? <Navigate to="/" /> : <ContactForm />} />
       </Routes>
     </BrowserRouter>
   );
