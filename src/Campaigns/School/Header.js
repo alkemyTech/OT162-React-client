1b5518d30175016c2912db47b1708988f2432c9b
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-04-25 00:52:10 -0300

    Added background color for smart tv and adjusted breakpoints

--- src/Campaigns/School/Header.js
+++ src/Campaigns/School/Header.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import { Grid } from '@mui/material';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 const Header = () => {
+  const theme = createTheme({
+    breakpoints: {
+      values: {
+        xs: 0,
+        sm: 600,
+        md: 900,
+        lg: 1200,
+        xl: 1920,
+      },
+    },
+  });
+
   return (
-    <Grid container sx={{padding: 5, justifyContent: 'space-between'}}>
-      <Grid item xs={12} sm={6} md={4} sx={{ margin: 'auto', justifyContent: "center", alignItems: "center"}}>
-        <img src='images/school-campaign/school-campaign-logo.png' height={150}/>
+    <ThemeProvider theme={theme}>
+      <Grid container sx={{padding: 5, justifyContent: 'space-between', backgroundColor: {xl: '#ff00008e'}}}>
+        <Grid item xs={12} sm={6} md={4} sx={{ margin: 'auto', justifyContent: "center", alignItems: "center"}}>
+          <img src='images/school-campaign/school-campaign-logo.png' height={150}/>
+        </Grid>
+        <Grid item md={4} sx={{ display: { xs: "none", md: "flex" }, justifyContent: "center", alignItems: "center"}}>
+          <h2>Juguetes por más sonrisas</h2>
+        </Grid>
+        <Grid item sm={6} md={4} sx={{ display: {xs: "none", sm: "block"}, justifyContent: "center", alignItems: "center" }}>
+          <img src='images/school-campaign/LOGO-SOMOS-MAS.png'/>
+        </Grid>
       </Grid>
-      <Grid item md={4} sx={{ display: { xs: "none", md: "flex" }, justifyContent: "center", alignItems: "center"}}>
-        <h2>Juguetes por más sonrisas</h2>
-      </Grid>
-      <Grid item sm={6} md={4} sx={{ display: {xs: "none", sm: "block"}, justifyContent: "center", alignItems: "center" }}>
-        <img src='images/school-campaign/LOGO-SOMOS-MAS.png'/>
-      </Grid>
-    </Grid>
+    </ThemeProvider>
   );
 }
  
