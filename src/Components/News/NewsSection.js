680894b9ded1d2a0ed26c80035cf2f0123ff1556
leocomeron <leocomeron@gmail.com>
2022-04-27 00:07:24 -0300

    Create search bar

--- src/Components/News/NewsSection.js
+++ src/Components/News/NewsSection.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import Title from "../Title/Title";
 import "./NewsSection.css";
 import Card from "../Cards/Card";
-import { Grid } from "@mui/material";
+import { Container, Grid, TextField, Box } from "@mui/material";
 import axios from "axios";
 import { errorAlert } from "../../features/alerts/alerts";
 import Loading from "../Utilities/Loading";
@@ -11,23 +11,39 @@ import LastEvent from "./LastEvent";
 const NewsSection = () => {
   const [news, setNews] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [newsSearch, setNewsSearch] = useState("");
   const baseRoute = process.env.REACT_APP_NEWS_ROUTE;
 
   useEffect(() => {
     setIsLoading(true);
-    axios
-      .get(baseRoute)
-      .then((res) => {
-        setNews(res.data.data);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        setNews({ message: err.toString(), severity: "error" });
-        errorAlert(err, "Invalid request", "Exit");
-        setIsLoading(false);
-      });
-  }, []);
+    if (newsSearch.length <= 2) {
+      axios
+        .get(baseRoute)
+        .then((res) => {
+          setNews(res.data.data);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          setNews({ message: err.toString(), severity: "error" });
+          errorAlert(err, "Invalid request", "Exit");
+          setIsLoading(false);
+        });
+    } else {
+      axios
+        .get(`${baseRoute}? search=${newsSearch}`)
+        .then((res) => {
+          setNews(res.data.data);
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          setNews({ message: err.toString(), severity: "error" });
+          errorAlert(err, "Invalid request", "Exit");
+          setIsLoading(false);
+        });
+    }
+  }, [newsSearch]);
 
+  console.log(newsSearch);
   return (
     <div>
       {isLoading && <Loading />}
@@ -35,36 +51,45 @@ const NewsSection = () => {
         <div>
           <Title
             title="Novedades"
-            text={
-              "Enterate de todas las novedades!"
-            }
-            img={
-              "./images/novedadesBackground.jpg"
-            }
+            text={"Enterate de todas las novedades!"}
+            img={"./images/novedadesBackground.jpg"}
           />
-
-          <Grid
-            container
-            justifyContent="space-evenly"
-            alignItems="stretch"
-            sx={{
-              margin: 3,
-              gap: "2em 0",
-              maxWidth: 1400,
-              margin: "0 auto",
-              padding: "0 2em",
-            }}
-          >
-            {news.map((item) => (
-              <Card
-                key={item.id}
-                image={item.image}
-                title={item.name}
-                description={item.content}
-                placeholder={item.placeholder}
+          <Container>
+            <Box>
+              <TextField
+                helperText="Min 3 caracteres"
+                label="Buscar novedades"
+                variant="filled"
+                onChange={(e) => {
+                  setNewsSearch(e.target.value);
+                }}
+                value={newsSearch}
               />
-            ))}
-          </Grid>
+            </Box>
+            {news.length == 0 && <p>No se encontraron novedades</p>}
+            <Grid
+              container
+              justifyContent="space-evenly"
+              alignItems="stretch"
+              sx={{
+                margin: 3,
+                gap: "2em 0",
+                maxWidth: 1400,
+                margin: "0 auto",
+                padding: "0 2em",
+              }}
+            >
+              {news.map((item) => (
+                <Card
+                  key={item.id}
+                  image={item.image}
+                  title={item.name}
+                  description={item.content}
+                  placeholder={item.placeholder}
+                />
+              ))}
+            </Grid>
+          </Container>
         </div>
       )}
       <LastEvent />
