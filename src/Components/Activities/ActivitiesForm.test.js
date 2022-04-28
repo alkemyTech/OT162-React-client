6d7f592631ac8c35503e0cb206d4221c981ea8ea
Michel San Martin <mikeemmanuel91@gmail.com>
2022-04-28 19:41:48 -0300

    added dummy test

--- src/Components/Activities/ActivitiesForm.test.js
+++ src/Components/Activities/ActivitiesForm.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import ActivitiesForm from './ActivitiesForm';
 
 // Render Test
 it('renders without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<ActivitiesForm/>, div)
+});
+
+// Test form submit
+test('form submit', () => {
+    const handleSubmit = jest.fn()
+    render(<ActivitiesForm onSubmit={handleSubmit}/>)
+    const user = userEvent.setup()
+
+    user.type(screen.getByLabelText("Titulo de la actividad", {exact: false}))
+    // user.type(screen.getByTestId('descriptionField', {exact: false}))
+    user.click(screen.getByRole('button', {name: "Enviar"}))
+
+    waitFor(() => 
+        expect(handleSubmit).toHaveBeenCalledWith({
+            image: null,
+            name: null,
+        }))
 })
