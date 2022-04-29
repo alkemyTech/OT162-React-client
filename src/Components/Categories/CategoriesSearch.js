2bbf0e2c3ba34d175c3ca473321e95216f5d98ed
Pedro Julian Palavecino <samuguayfenix@gmail.com>
2022-04-29 18:57:25 -0300

    Implemented debounce effect in categories search component

--- src/Components/Categories/CategoriesSearch.js
+++ src/Components/Categories/CategoriesSearch.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card } from '@mui/material';
 import { Formik } from 'formik';
 import SearchIcon from '@mui/icons-material/Search';
 import { getCategoriesSlice, filterCategorySlice } from "../../features/categories/categoriesSlice";
 import { useDispatch } from "react-redux";
+import useDebounce from "../../hooks/useDebounce";
 
 const CategoriesSearch = () => {
     const dispatch = useDispatch();
+    const [search, setSearch] = useState("");
+    const debouncedSearch = useDebounce(search, 500)
+
+    useEffect(() => {
+        if (debouncedSearch.length < 4) {
+            dispatch(getCategoriesSlice());
+            console.log("send less than 4");
+        } else {
+            dispatch(filterCategorySlice(debouncedSearch));
+            console.log("send");
+        }
+    }, [debouncedSearch, dispatch])
+
     return (
         <Card sx={{ minWidth: 650, maxWidth: 1000, minHeight: 50, marginLeft: 'auto', marginRight: 'auto', display: 'flex', padding: 2 }}>
             <SearchIcon/>
             <Formik
                 initialValues={{ search: '' }}
-                onSubmit={(values) => {
-                    if (values.search.length < 4) {
-                        dispatch(getCategoriesSlice());
-                    } else {
-                        dispatch(filterCategorySlice(values.search));
-                    }
-                }}
+                onSubmit={(values) => setSearch(values.search)}
             >
                 {({
                     handleChange,
