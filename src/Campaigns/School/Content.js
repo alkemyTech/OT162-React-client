d500098244f9dbf163b3a0e8fa3b690724ad34ff
César Carabetta <carabetta87@gmail.com>
2022-04-30 18:37:40 -0300

    Finished feature 132

--- src/Campaigns/School/Content.js
+++ src/Campaigns/School/Content.js
@@ -10,16 +10,28 @@ const Content = () => {
 
   return (
     <>
-    <div>
+    <div className='sc_container'>
+      <div>
       <p style={{fontSize: "1.5rem"}}>
         Campaña escolar <b>"Juntos por la educación"</b>: Recolectamos donaciones para los chicos que más necesitan materiales para poder seguir estudiando. ¡Te esperamos!
       </p>
-      <span><b>Fecha</b>: 30/6/2022</span><br></br>
-      <span><b>Hora</b>: 15hs</span><br></br>
-      <span><b>Sitio</b>: Parque centenario</span>
-    </div>
-    <div className='counter'>
-      <span><b>Faltan</b>: {days} días, {hours} hrs {minutes} min {seconds} seg</span>
+      <div className='sc_container__middle'>
+        <div>
+          <img src='images/school-campaign/blog-img-01.jpg' alt='parque centenario'></img>
+        </div>
+        <div className='sc_container__middle--data'>
+          <span><b>Fecha</b>: 30/6/2022</span><br></br>
+          <span><b>Hora</b>: 15hs</span><br></br>
+          <span><b>Sitio</b>: Parque centenario</span>
+        </div>
+        <div>
+          <img src='images/school-campaign/school-campaign-logo.png' alt='parque centenario'></img>
+        </div>
+      </div>
+      </div>
+      <div className='counter'>
+        <span><b>Faltan</b>: {days} días, {hours} hrs {minutes} min {seconds} seg</span>
+      </div>
     </div>
     </>
   );
