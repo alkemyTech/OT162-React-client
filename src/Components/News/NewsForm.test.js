18b92e0b1f62a35b95a2fdb3bc365f3bd90a0235
juancruzsoto <juancruzlp2013@gmail.com>
2022-05-02 23:26:50 -0300

    first test working

--- src/Components/News/NewsForm.test.js
+++ src/Components/News/NewsForm.test.js
@@ -1,56 +1,71 @@
-import {
-    render,
-    wait,
-  } from "@testing-library/react";
-  import NewsForm from "./NewsForm";
-  import "@testing-library/jest-dom";
-  import userEvent from "@testing-library/user-event";
-  import axiosMock from "axios";
-  
-  // jest.mock("axios");
-  jest.setTimeout(10000);
-  
-  describe("News Form Test", () => {
-    test("Validate fields before submit", async () => {
-      const { getByTestId, getByText } = render(<NewsForm />);
-  
-      await wait(() => {
-        userEvent.click(getByTestId("buttonSend"));
-      });
-  
-      const error = getByText("Required");
-      await wait(() => expect(error).toBeInTheDocument());
-    });
-    // test("Validate successfull HTTP request", async () => {
-    //   const { getByTestId, getByText, getByRole } = render(<NewsForm />);
-  
-    //   userEvent.type(getByTestId("titleTest"), "Titulo Test");
-    //   userEvent.type(getByTestId("contentTest"), "Contenido Test");
-    //   userEvent.type(getByTestId("imageTest"), "Imagen Test");
-    //   userEvent.type(getByTestId("categoryTest"), "Categoria Test");
-  
-    //   await wait(() => {
-    //     userEvent.click(getByRole("button"));
-    //     axiosMock.post.mockResolvedValueOnce();
-    //   });
-  
-    //   const success = getByText("Categoria Agregada con Exito!");
-    //   await wait(() => expect(success).toBeInTheDocument());
-    // });
+import { render, waitFor } from "@testing-library/react";
+import NewsForm from "./NewsForm";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import { BrowserRouter } from "react-router-dom";
 
-    // test("Validate wrong HTTP request", async () => {
-    //   const { getByTestId, getByText } = render(<NewsForm />);
-  
-    //   userEvent.type(getByTestId("titleTest"), "Titulo Test");
-    //   userEvent.type(getByTestId("contentTest"), "Contenido Test");
-    //   userEvent.type(getByTestId("imageTest"), "Imagen Test");
-    //   userEvent.type(getByTestId("categoryTest"), "Categoria Test");
-  
-    //   await wait(() => {
-    //     userEvent.click(getByText("Enviar"));
-    //   });   
-  
-    //   const error = getByText("No es posible agregar Categoria");
-    //   await wait(() => expect(error).toBeInTheDocument());
-    // });
+describe("News Form Test", () => {
+  const initialState = { news: { status: "", data: { id: "" } } };
+  const mockStore = configureStore();
+  let store;
+
+  test("Validate fields before submit", async () => {
+    store = mockStore(initialState);
+    const { getByTestId, getAllByText } = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <NewsForm />
+        </BrowserRouter>
+      </Provider>
+    );
+
+    await waitFor(() => {
+      userEvent.click(getByTestId("buttonSend"));
+    });
+
+    const error = getAllByText("Required");
+    await waitFor(() => expect(error[0]).toBeInTheDocument());
   });
+
+  // test("Validate successfull HTTP request", async () => {
+  //   const { getByTestId, getByText, findByText } = render(
+  //     <Provider store={store}>
+  //       <BrowserRouter>
+  //         <NewsForm />
+  //       </BrowserRouter>
+  //     </Provider>
+  //   );
+
+  //   userEvent.type(getByTestId("titleTest"), "Titulo Test");
+  //   // userEvent.type(getByTestId("contentTest"), "Contenido Test");
+  //   userEvent.type(getByTestId("imageTest"), "Imagen Test");
+  //   userEvent.type(getByTestId("categoryTest"), "Categoria Test");
+
+  //   // await waitFor(() => {
+  //   //   userEvent.click(getByRole("button"));
+  //   //   axiosMock.post.mockResolvedValueOnce();
+  //   // });
+
+  //   const success = findByText("Noticia creada");
+  //   console.log("aaaaa", success);
+  //   await waitFor(() => expect(success).toBeInTheDocument());
+  // });
+
+  // test("Validate wrong HTTP request", async () => {
+  //   const { getByTestId, getByText } = render(<NewsForm />);
+
+  //   userEvent.type(getByTestId("titleTest"), "Titulo Test");
+  //   userEvent.type(getByTestId("contentTest"), "Contenido Test");
+  //   userEvent.type(getByTestId("imageTest"), "Imagen Test");
+  //   userEvent.type(getByTestId("categoryTest"), "Categoria Test");
+
+  //   await wait(() => {
+  //     userEvent.click(getByText("Enviar"));
+  //   });
+
+  //   const error = getByText("No es posible agregar Categoria");
+  //   await wait(() => expect(error).toBeInTheDocument());
+  // });
+});
